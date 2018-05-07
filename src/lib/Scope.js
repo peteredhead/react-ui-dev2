717717d76a9845a5ad995df9d3be3f1d2d294922
Pete Redhead <pete.redhead@lobsterpictures.tv>
2018-05-08 06:52:26 +1200

    Added PI scope

--- src/lib/Scope.js
+++ src/lib/Scope.js
@@ -1,8 +1,44 @@
+import moment from 'moment'
+
 export default class Scope {
 
     constructor() {
-
+        this.startTime = undefined
+        this.stopTime = undefined
+        this.serviceScopes = []
     }
 
-    // serviceScope
+    parse(element) {
+        if (element.hasAttribute("startTime")) {
+            try {
+                startTime = moment(element.getAttribute("startTime"))
+                this.startTime = startTime
+            } catch(err) {
+                console.warn("Scope has invalid start time")
+            }
+        }
+        if (element.hasAttribute("stopTime")) {
+            try {
+                stopTime = moment(element.getAttribute("stopTime"))
+                this.stopTime = stopTime
+            } catch(err) {
+                console.warn("Scope has invalid stop time")
+            }
+        }
+        var childNodes = element.childNodes
+        for (var i=0; i < childNodes.length; i++) {
+            const child = childNodes[i]
+            if (child.nodeName === "serviceScope") {
+                this.parseServiceScope(child)
+            }
+        }
+    }
+
+    parseServiceScope(element) {
+        if (element.hasAttribute("id")) {
+            this.serviceScopes.push(element.getAttribute("id"))
+        } else {
+            console.warm("Service scope without an ID")
+        }
+    }
 }
